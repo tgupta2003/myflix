197c5da0efabe1c7c34b74d62f44d974e4635152
tgupta30 <trishagupta368@gmail.com>
2023-12-08 01:37:15 -0500

    Added files

---
+++ person1.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function(event){
+
+	  observer.observe();
+
+
+
+})
+
+
+// for buttons
+function myAlert() {
+  alert('Unable to stream');
+}
+
+
+function myAlert2(){
+	alert('Could not find information');
+}
+
+function goProfile(){
+	window.location.href = 'profile.html';
+}
+
+function goHome(){
+	window.location.href = 'index.html'; 
+}
+
+function content(){
+	alert('Cannot access content at this time');
+}
+
+function person1(){
+	window.location.href = 'person1.html'; 
+}
+
+
+// function welcome(){
+// 	 window.onload = function() {
+//    alert('Admin access granted for the following profiles:');
+// }
---
+++ person2.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function(event){
+
+	  observer.observe();
+
+
+
+})
+
+
+// for buttons
+function myAlert() {
+  alert('Unable to stream');
+}
+
+
+function myAlert2(){
+	alert('Could not find information');
+}
+
+function goProfile(){
+	window.location.href = 'profile.html';
+}
+
+function content(){
+	alert('Cannot access content at this time');
+}
+
+function goHome(){
+	window.location.href = 'index.html'; 
+}
